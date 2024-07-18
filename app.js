fd1d8899fbdf32e3690760f664dbbcc71ef8532b
Andre <opolloandres@gmail.com>
2024-07-18 11:49:30 +0300

    Api done

---
+++ app.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+require('express-async-errors')
+const express = require('express')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+const notFoundMiddleware = require('./middleware/not-found')
+const authenticatedUser =require('./middleware/auth')
+const cors = require('cors')
+const helmet = require('helmet')
+const rateLimiter = require('express-rate-limit')
+const app = express()
+//DB
+const connectDB = require('./db/connect')
+
+//router
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
+app.use(express.json())
+app.use(cors())
+app.use(helmet())
+app.set('trust proxy',1)
+app.use(rateLimiter({
+    windowsMs:15 * 60 * 1000, //15mins
+    max:100
+}))
+
+//routes
+app.use('/api/v1/auth',authRouter)
+app.use('/api/v1/jobs',authenticatedUser,jobsRouter)
+//middleware
+app.use(errorHandlerMiddleware)
+app.use(notFoundMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async ()=>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port: ${port}`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+start()
+
---
+++ middleware/not-found.js
@@ -0,0 +1,3 @@
+const notFoundMiddleware = (req,res)=>{ return res.status(404).send("Route does not exist")}
+
+module.exports = notFoundMiddleware
